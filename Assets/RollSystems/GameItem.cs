13e5d49a25643e9fd99cafb6a2a02a6f2f6d76d9
Marc Meugniot <marcso@think007.com>
2015-11-30 21:03:02 -0800

    InitialCommit

---
+++ Assets/RollSystems/GameItem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace RollSystems
+{
+    [Serializable]
+    public class GameItemCollection
+    {
+
+        public int Min;
+        public int Max;
+        public ItemCollectionTypes CollectionType;
+        public List<GameItem> GameItems;
+
+    }
+    [Serializable]
+    public class GameItem
+    {
+        public GameItemTypes Type = GameItemTypes.Other;
+        public int HP = 100;
+        public int Atack = 10;
+        public int Defence = 10;
+        public List<GameObject> Items;
+    }
+
+    public enum ItemCollectionTypes
+    {
+        Floor,
+        Edge,
+        SoftWall,
+        Wall,
+        HardWall,
+        SecretPassage,
+        Other
+    }
+
+    public enum GameItemTypes
+    {
+        Food,
+        Enemy,
+        Ally,
+        Terrain,
+        Other
+    }
+
+}
